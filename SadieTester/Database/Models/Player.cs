23b66552e2e25ad92502155898a3d52b9e5b82d8
ashdevelops <ash.smith.dev@gmail.com>
2025-07-29 19:19:27 +0100

    cleanup

--- SadieTester/Database/Models/Player.cs
+++
@@ -1,9 +0,0 @@
-namespace SadieTester.Database.Models;
-
-public class Player
-{
-    public int Id { get; set; }
-    public string Username { get; set; }
-    public PlayerData Data { get; set; }
-    public List<PlayerSsoToken> Tokens { get; set; } = [];
-}
